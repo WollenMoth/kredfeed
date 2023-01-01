f789c26f42bff16e5a8b8a8264299e7b871ecf17
André Díaz <andre290800@gmail.com>
2022-12-31 20:08:10 -0600

    feat: ✨ user model

---
+++ src/models/user.ts
@@ -0,0 +1,34 @@
+export interface Domicilio {
+  calle: string;
+  numExt: number;
+  numInt: number;
+  codPost: number;
+  colonia: string;
+  municipio: string;
+  ciudad: string;
+  estado: string;
+  pais: string;
+}
+
+export interface Empresa {
+  correo: string;
+  nombre: string;
+  fechaConst: Date;
+  domicilio: Domicilio;
+  compDomicilio: File | null;
+}
+
+export interface Representante {
+  correo: string;
+  nombre: string;
+  nacionalidad: string;
+  fechaNac: Date;
+  tel: string;
+  domicilio: Domicilio;
+  identificacion: File | null;
+}
+
+export default interface User {
+  empresa: Empresa;
+  representante: Representante;
+}
